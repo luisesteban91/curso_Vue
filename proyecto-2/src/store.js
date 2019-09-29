0d2a9c1a461a6b572b65034e46bb5a5fb227a6dd
unknown <luisesteban.cch@gmail.com>
2019-09-29 13:09:47 -0500

    deploy first commit

---
+++ proyecto-2/src/store.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    frutas:[
+      {nombre: 'manzana', 'cantidad': 0},
+      {nombre: 'platano', 'cantidad': 0},
+      {nombre: 'pera', 'cantidad': 0}
+    ]
+  },
+  mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad++
+    },
+    reiniciar(state){
+      state.frutas.forEach(frutas => {
+        frutas.cantidad = 0
+      })
+    }
+  },
+  actions: {
+    
+  }
+})
